e40454f306536dc97bc55eca588be52d2926fddd
Runze Zhang <23548024+zj713300@users.noreply.github.com>
2017-10-12 08:53:58 +0800

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -413,5 +413,106 @@ var textStrings_ES = {
     "misatrributed"      : "(mal atribuido)" ,
 };
 
+var textStrings_CN = {
+    // Act 1
+    "niceHat"              : "流行时尚"                                          ,
+    "outtaHere"            : "滚出去！"                                ,
+    "getARoom"             : "恶心，去开房吧"                                  ,
+    "notCoolAnymore"       : "帽子已经不流行了"                          ,
+    "tvOnTv"               : "电视在电视里！"                                        ,
+    "cricky"               : "小蟋蟀"                                        ,
+    "tooManyCrickets"      : "蟋蟀泛滥！"                         ,
+    "normalPeep"           : "没有什么大新闻"                                    ,
+    "normalPeeps"          : "没有什么大新闻"                                ,
+    "wowNothing"           : "什么都没有拍到"                                  ,
+    
+    // Act 2
+    "crazySquareAttacks"   : "疯狂的方方"                                 ,
+    "justMissed"           : "错过了精彩瞬间"                                 ,
+    "somethingInteresting" : "(拍出他搞事情的画面)" ,
+    "whoIsScreaming"       : "(谁吓到了他)"               ,
+    "circleFearsSquares"   : "圆圆害怕方方"                                  ,
+    "whoScaresThem"        : "(谁在害怕他们)"         ,
+    "areTheyScared"        : "(ya gotta catch 'em *being* scared by a square)"       , // not used
+    "squaresSnubCircles"   : "方方不喜欢圆圆"                                  ,
+    "areTheySnubbed"       : "(ya gotta catch 'em *while* snubbing a circle)"        , // not used
+    "everyoneHates"        : "仇恨蒙蔽了所有人的眼睛"                           ,
+    "almostEveryoneHates"  : "几乎每个人都讨厌对方"                     ,
+    "squaresHateCircles"   : "方方讨厌圆圆"                                  ,
+    "circlesHateSquares"   : "圆圆讨厌方方"                                  ,
+    "areTheyYelling"       : "(ya gotta catch 'em *yelling* at others)"              , // not used
+    "nerdsNow"             : "这些书呆子们在做什么"                        ,
+    "schockedPeep"         : "这有什么好震惊的？"                              ,
+    "whatever"             : "随你咯╭(╯^╰)╮"                                              ,
+    
+    // Act 3
+    "ellipsis"             : ". . ."                                                 ,
+    "coolNoMore"           : "你太土了！听不见吗？"           ,
+    "beScared"             : "害怕吧，愤怒吧。"                                  ,
+    
+    // MANIFESTO
+    "manifesto" : [
+       //"as if you viewers want GOOD news"       ,
+       "谁会看这种频道？",
+       "和平很无趣，暴力才能制造新闻。"    ,
+       //"peace is boring. conflict gets clicks." ,
+       "每个故事都需要一个冲突作为楔子，所以..."  ,
+       //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+       "给观众他们想要的东西"
+    ] ,
+    
+    // -- Strings currently in graphics resources --
+    
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "游戏时间：5分钟" ,
+    "warning"            : "警告：下列场景\n包含势利\n粗暴，以及大屠杀\n观众最好拥有判断力" ,
+    
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "拍摄镜头" ,
+    
+    // (chyron3.png)
+    "chyronNothing"      : "什么都没有拍到" ,
+    
+    // (credits0001.png)
+    "createdBy"          : "制作：" ,
+    "NickyCase"          : "Nicky Case" ,
+    
+    // (credits0002.png)
+    "manyThanks"         : "感谢测试本游戏的人：" ,
+    
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "以及捐款支持我的人：" ,
+    
+    // (credits0007.png)
+    "lastButNotLeast"    : "最后，最重要的是：" ,
+    
+    // (credits0008.png)
+    "thankYouForPlaying" : "感谢玩家们。" ,
+    
+    // (preload_play.png)
+    "playButton"         : "PLAY" ,
+    
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "看看我的其他作品" ,
+    "buyCoffeeButton"    : "支持我" ,
+    "replayButton"       : "再玩一次" ,
+    
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "分享：" ,
+    
+    // (end_prototype.png)
+    "endOfPrototype"     : "END OF PROTOTYPE" ,
+    "toBeContinued"      : "(to be continued!)" ,
+    
+    // (quote0002.png)
+    "WBWWB"              : "我们终将成为我们所关注的那样。" ,
+    "WSOTATOTSU"         : "我们塑造工具，工具也塑造我们。" ,
+    
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "马歇尔·麦克卢汉" ,
+    
+    // (quote0004.png)
+    "misatrributed"      : "(并不是他说的↑)" ,
+};
 
-var textStrings = textStrings_EN
+var textStrings = CN

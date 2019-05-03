ed6201e6fe4f95441929e7860159569979629a16
david082321 <7046617+david082321@users.noreply.github.com>
2019-05-03 22:45:03 +0800

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -7,27 +7,27 @@ var textStrings = {
     "tvOnTv"               : "電視在電視裡！"                                        ,
     "cricky"               : "小蟋蟀"                                        ,
     "tooManyCrickets"      : "蟋蟀泛濫！"                         ,
-    "normalPeep"           : "並沒有什麼大新聞"                                    ,
-    "normalPeeps"          : "並沒有什麼大新聞"                                ,
-    "wowNothing"           : "什麼都沒有拍到"                                  ,
+    "normalPeep"           : "並沒有什麼大新聞　　"                                    ,
+    "normalPeeps"          : "並沒有什麼大新聞　　"                                ,
+    "wowNothing"           : "什麼都沒有拍到　　"                                  ,
     
     // Act 2
     "crazySquareAttacks"   : "瘋狂的方方"                                 ,
     "justMissed"           : "錯過了精彩瞬間"                                 ,
-    "somethingInteresting" : "拍出他搞事的畫面" ,
+    "somethingInteresting" : "拍出他搞事的畫面　　" ,
     "whoIsScreaming"       : "誰嚇到了他"               ,
     "circleFearsSquares"   : "圓圓害怕方方"                                  ,
     "whoScaresThem"        : "誰在害怕他們"         ,
-    "areTheyScared"        : "拍方方吼他們的照片"       , // not used
+    "areTheyScared"        : "拍方方吼他們的照片　　"       , // not used
     "squaresSnubCircles"   : "方方不喜歡圓圓"                                  ,
-    "areTheySnubbed"       : "拍圓圓吼他們的照片"        , // not used
-    "everyoneHates"        : "仇恨蒙蔽人們的雙眼"                           ,
-    "almostEveryoneHates"  : "大多數人都討厭對方"                     ,
-    "squaresHateCircles"   : "方方討厭圓圓"                                  ,
-    "circlesHateSquares"   : "圓圓討厭方方"                                  ,
-    "areTheyYelling"       : "拍出他們吼人的照片"              , // not used
+    "areTheySnubbed"       : "拍圓圓吼他們的照片　　"        , // not used
+    "everyoneHates"        : "仇恨蒙蔽了人們的雙眼　　"                           ,
+    "almostEveryoneHates"  : "大多數人都討厭對方　　"                     ,
+    "squaresHateCircles"   : "方方討厭圓圓　　"                                  ,
+    "circlesHateSquares"   : "圓圓討厭方方　　"                                  ,
+    "areTheyYelling"       : "拍出他們吼人的照片　　"              , // not used
     "nerdsNow"             : "壞蛋們在做什麼"                        ,
-    "schockedPeep"         : "這有什麼好驚訝的？"                              ,
+    "schockedPeep"         : "這有什麼好驚訝的？　　"                              ,
     "whatever"             : "隨你咯╭(╯^╰)╮"                                              ,
     
     // Act 3
@@ -39,9 +39,9 @@ var textStrings = {
     "manifesto" : [
        //"as if you viewers want GOOD news"       ,
        "誰會看這種頻道？",
-       "和平很無趣，暴力才是新聞。"    ,
+       "和平很無趣，暴力才是新聞。　　　　　"    ,
        //"peace is boring. conflict gets clicks." ,
-       "每個故事都需要衝突作引子..."  ,
+       "每個故事都需要衝突作引子...　　　　"  ,
        //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
        "給觀眾他們想要的"
     ] ,
